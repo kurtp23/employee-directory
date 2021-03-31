4600bf6f34bce68f1e49df1c9d1f2e9a854fb815
kurtp23 <kurt_phillips@live.com>
2021-03-31 14:51:45 -0700

    working sort console.log

--- src/components/MainDiv.js
+++ src/components/MainDiv.js
@@ -4,49 +4,58 @@ import Cards from "./card.js";
 // import For from "react-loops";
 function MainDiv() {
   const [directory, setDirectory] = useState([]);
-  const [dString, setDString] = useState("");
 
   useEffect(() => {
     Directory.search().then((data) => {
-      // console.log(data.data.results);
-
       const employees = data.data.results;
-      let r = "";
-
-      employees.forEach((element) => {
-        r += element.name.first;
-        r += element.email;
-        r += "\n";
-      });
 
       setDirectory(employees);
-      setDString(r);
+      console.log(directory);
     });
-    return "hello";
-  });
-  console.log(directory);
+  }, []);
+  const handleSort = () => {
+    console.log("hello");
+    const newDirectory = directory.sort((a, b) => a.name.first.localeCompare(b.name.first));
+    console.log(directory.sort((a, b) => a.name.first.localeCompare(b.name.first)));
+    setDirectory(newDirectory);
+    console.log("hello");
+  };
 
   return (
     <>
       <nav>
-        {/* {this.displayEmployee()} */}
         <div className="nav-wrapper center-align">
           Employee Directory
           <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <button
-              onClick={(event) => {
-                event.preventDefault();
-                // callApi();
-              }}
-            >
-              employee
-            </button>
+            <ul id="dropdown2" className="dropdown-content">
+              <li>
+                <a href="#!">
+                  one<span className="badge">1</span>
+                </a>
+              </li>
+              <li>
+                <a href="#!">
+                  two<span className="new badge">1</span>
+                </a>
+              </li>
+              <li>
+                <a href="#!">three</a>
+              </li>
+            </ul>
+            <a className="btn dropdown-trigger" href="#!" data-target="dropdown2">
+              Dropdown<i className="material-icons right">arrow_drop_down</i>
+            </a>
             <li></li>
             <li></li>
+            <button onClick={handleSort}>sort alphebetically</button>
           </ul>
         </div>
       </nav>
-      <div className="row">{Cards(directory)}</div>
+      <div className="row">
+        {directory.map((person) => (
+          <Cards person={person} />
+        ))}
+      </div>
     </>
   );
 }
--- src/components/card.js
+++ src/components/card.js
@@ -1,39 +1,35 @@
 import React from "react";
 
-function Cards(arr) {
+function Cards({ person }) {
   return (
-    <>
-      {arr.map((person) => {
-        return (
-          <div className="col s4 m3">
-            <div className="card small center-align">
-              <div className="card-image ">
-                <img
-                  style={{
-                    height: 200,
-                    width: 232,
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                  alt="employee "
-                  src={person.picture.large}
-                />
-              </div>
-              <div className="card-content">
-                <p>
-                  {person.name.first} {person.name.last}
-                  <br></br>
-                  {person.email} {person.phone}
-                  <br></br>
-                  location: {person.location.city},{person.location.state},{person.nat}
-                </p>
-                <ul></ul>
-              </div>
-            </div>
+    <div>
+      <div className="col s4 m3" key={person.phone}>
+        <div className="card small center-align">
+          <div className="card-image ">
+            <img
+              style={{
+                height: 200,
+                width: 232,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              alt="employee "
+              src={person.picture.large}
+            />
           </div>
-        );
-      })}
-    </>
+          <div className="card-content">
+            <p>
+              {person.name.first} {person.name.last}
+              <br></br>
+              {person.email} {person.phone}
+              <br></br>
+              location: {person.location.city},{person.location.state},{person.nat}
+            </p>
+            <ul></ul>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
